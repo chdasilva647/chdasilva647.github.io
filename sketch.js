f123b517adce6890dd56cb2f0f7cf654ab6257dc
chdasilva647 <chloedasilva647@gmail.com>
2025-04-11 22:02:23 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,14 +2,14 @@ var x = 0;
 var y = 0;
 function setup(){
     createCanvas(720, 480); 
-    //background(200); // 0-255 is black, white and greyscale in between
+    canvas.position(325, 800); 
 }
 
 function draw(){
-    background('#000000') // for a hex hode, u need a # // automatic semicolon insertion
+    background('#000000') 
     circle(x,y,10,10);
     x = x + 1;
-    x = x % 600; //modulo operator
+    x = x % 600; 
     y = y + 2;
     y = y % 480;
 }
